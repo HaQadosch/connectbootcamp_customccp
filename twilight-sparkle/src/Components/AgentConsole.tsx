35ad0a2427793e35f51a45dcbc1791efe456b2fe
Xavier Brinon <xavier.brinon@ecs.co.uk>
2019-07-03 17:21:57 +0100

    grid template

--- twilight-sparkle/src/Components/AgentConsole.tsx
+++ twilight-sparkle/src/Components/AgentConsole.tsx
@@ -6,7 +6,9 @@ interface IAgentConsoleProps {}
 export const AgentConsole: React.FC<IAgentConsoleProps> = props => {
   return (
     <div className='agentConsole' data-testid='agentConsole'>
-      AgentConsole
+      <h1>welcome to amazon connect agent console</h1>
+      <section id='ccpContainer'></section>
+      <section id='section-main'></section>
     </div>
   );
 };
