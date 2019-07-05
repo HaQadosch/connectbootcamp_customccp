5162ad8b5602e70dcf63b57b4f2eacd6887811a1
Xavier Brinon <xavier.brinon@ecs.co.uk>
2019-07-05 15:06:20 +0100

    typesetting contact api

--- twilight-sparkle/src/Components/AgentConsole.tsx
+++ twilight-sparkle/src/Components/AgentConsole.tsx
@@ -7,18 +7,70 @@ declare global {
   interface Window {
     connect: {
       core: {
-        initCCP: (container: React.MutableRefObject<HTMLElement> | null, init: unknown) => void;
+        initCCP: (container: React.MutableRefObject<HTMLElement> | HTMLElement | null, init: unknown) => void;
       };
+      contact: (arg: ContactedT) => void;
     };
   }
 }
 
+type ContactedT = {
+  onConnecting: (arg: OnConnectedT) => void;
+};
+
+type OnConnectedT = {
+  contactId: string;
+};
+
+const initCCP: (
+  init: unknown,
+) => (container: React.MutableRefObject<HTMLElement> | HTMLElement | null) => void = initConf => anchorTag =>
+  window.connect.core.initCCP(anchorTag, initConf);
+const initConf = {
+  ccpURL: 'https://ecscc-demo.awsapps.com/connect/ccp#/',
+  softphone: {
+    allowFramedSoftphone: true,
+    disabledRingtone: false,
+  },
+};
+
+const listenToContact = () => {
+  console.log('listenToContact');
+  const onConnectingCB = (onConnected: OnConnectedT): void => {
+    const { contactId, ...rest } = onConnected;
+    console.log('onConnecting', { contactId, rest });
+  };
+  const contactCB = (contacted: ContactedT): void => {
+    console.log('contact', { contacted });
+    contacted.onConnecting(onConnectingCB);
+  };
+  const foo = (onConnected: OnConnectedT) => {
+    const { contactId, ...rest } = onConnected;
+    console.log({ contactId, rest });
+  };
+  // window.connect.contact(contactCB)
+  // window.connect.contact(contacted => {
+  // console.log('contact', { contacted })
+  // contacted.onConnecting(onConnected => {
+  //   console.log('onConnecting', { onConnected })
+  // })
+  // })
+};
+
 export const AgentConsole: React.FC<IAgentConsoleProps> = props => {
   const ccpContainerRef = useRef(null);
+  // const ccpDom = window.document.getElementById('ccpContainer')
 
   useEffect(() => {
     console.log('useEffect', { connect: window.connect });
-    window.connect.core.initCCP(ccpContainerRef.current, {});
+    try {
+      if (window.connect && ccpContainerRef) {
+        initCCP(initConf)(ccpContainerRef.current);
+        listenToContact();
+      }
+    } catch (error) {
+      console.error({ error, ccpContainerRef });
+    }
   }, []);
 
   return (
